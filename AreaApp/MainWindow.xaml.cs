783ae4271d0d1fa6e703a4cdd6d223f61f310ac7
Cal Alstad <Cal.Alstad@elev.ga.ntig.se>
2025-02-10 14:57:41 +0100

    vi har tränat på event

---
+++ AreaApp/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AreaApp;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void RäknaUt(object sender, RoutedEventArgs e)
+    {
+        int Bredd = int.Parse(vBredd.Text);
+        int Höjd = int.Parse(vHöjd.Text);
+
+        int Area = Bredd * Höjd;
+
+        Resultat.Text = Area.ToString();
+    }
+
+    private void Clear(object sender, RoutedEventArgs e)
+    {
+        vBredd.Text = "".ToString();
+        vHöjd.Text = "".ToString();
+        Resultat.Text = "".ToString();
+    }
+}
